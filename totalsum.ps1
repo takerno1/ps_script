a694ff8af1cdf93011c2a03759a98a727adc522a
Dmitrii <44246863+takerno1@users.noreply.github.com>
2019-07-09 07:05:06 -0500

    totalsum.ps1
    Total bytes per customer id parsed from a log file.

---
+++ totalsum.ps1
@@ -0,0 +1,27 @@
+$input = Get-Content C:\work\1.txt
+$VarName -as[int]
+$array = @()
+$input | foreach-object {
+    $writeobj = $false
+    $obj = New-Object System.Object
+    If ($_ -match 'CustomerId*') {
+        $CustomerId = ($_ -split 'CustomerId:' -split 'downloaded')[1]
+    }
+    If ($_ -match 'Size*') {
+        $Size = ($_ -split 'Blah Size:' -split 'bytes')[1]
+        $Size=[Double] $Size
+        $writeobj = $true
+    }
+    If ($writeobj){
+        $obj | Add-Member -type NoteProperty -name CustomerId -value $CustomerId
+        $obj | Add-Member -type NoteProperty -name Total_bytes -value $Size 
+        $array += $obj
+    }
+    $array | Group-Object CustomerId | %{
+    New-Object psobject -Property @{
+        Item = $_.Name
+        Sum = ($_.Group | Measure-Object Total_bytes -Sum).Sum
+    }
+}}
+
+$array
